df13f355c2f099bb5f122e6073c936f6b14f7472
lianatime <liana.vanrensburg@gmail.com>
2021-02-23 16:56:22 +1300

    User Entity, Web Tokens, Authentication

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using Domain;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -17,20 +16,24 @@ namespace API
         public static async Task Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
+
             using var scope = host.Services.CreateScope();
+
             var services = scope.ServiceProvider;
 
-            try
+            try 
             {
                 var context = services.GetRequiredService<DataContext>();
+                var userManager = services.GetRequiredService<UserManager<AppUser>>();
                 await context.Database.MigrateAsync();
-                await Seed.SeedData(context);
+                await Seed.SeedData(context, userManager);
             }
             catch (Exception ex)
             {
                 var logger = services.GetRequiredService<ILogger<Program>>();
-                logger.LogError(ex, "An Error occured during migration");
+                logger.LogError(ex, "An error occured during migraiton");
             }
+
             await host.RunAsync();
         }
 
--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -3,13 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Domain;
+using Microsoft.AspNetCore.Identity;
 
 namespace Persistence
 {
     public class Seed
     {
-        public static async Task SeedData(DataContext context)
+        public static async Task SeedData(DataContext context, UserManager<AppUser> userManager)
         {
+            if (!userManager.Users.Any())
+            {
+                var users = new List<AppUser>
+                {
+                    new AppUser{DisplayName = "Liana", UserName = "liana", Email = "liana@test.com"},
+                    new AppUser{DisplayName = "Kate", UserName = "kate", Email = "kate@test.com"},
+                    new AppUser{DisplayName = "Logan", UserName = "logan", Email = "logan@test.com"},
+                    new AppUser{DisplayName = "Drake", UserName = "drake", Email = "drake@test.com"},
+                };
+
+                foreach (var user in users)
+                {
+                    await userManager.CreateAsync(user, "Pa$$w0rd");
+                }
+            }
+
             if (context.Activities.Any()) return;
             
             var activities = new List<Activity>
