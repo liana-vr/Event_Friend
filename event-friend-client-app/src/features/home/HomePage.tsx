7fd89b85e3d2f4772771bffd46a9333c3605d86f
lianatime <liana.vanrensburg@gmail.com>
2021-02-28 16:10:41 +1300

    Infinite scroll added

--- event-friend-client-app/src/features/home/HomePage.tsx
+++ event-friend-client-app/src/features/home/HomePage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Header, Segment, Image, Button } from 'semantic-ui-react';
 import { useStore } from '../../app/stores/store';
@@ -6,8 +6,8 @@ import { observer } from 'mobx-react-lite';
 import LoginForm from '../users/LoginForm';
 import RegisterForm from '../users/RegisterForm';
 
-
 export default observer(function HomePage(){
+    
     const {userStore, modalStore} = useStore();
     return(
            <Segment inverted textAlign='center' vertical className='masthead'>
